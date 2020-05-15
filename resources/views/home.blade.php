e2d8f7252f3cf96b81d34e5e0455d6f9fc98c880
Marshall Oliver <olivermt91@gmail.com>
2020-05-15 09:47:29 -0400

    Initial Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('content')
+
+<div class="section">
+	<div class="container">
+
+		<h1 class="title">Laravel Dynamic DB</h1>
+		<p class="subtitle">
+			A simple Laravel project to enable connections to secondary databases using connection information stored in the primary Laravel database.
+		</p>
+
+	</div>
+</div>
+
+@endsection
